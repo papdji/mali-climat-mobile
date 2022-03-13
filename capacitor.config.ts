60b4fb35b343b9576705961f265664d56154515d
papdji <mbah0397@gmail.com>
2022-03-13 05:00:45 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Mali Climat',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
